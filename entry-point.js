e714a7de2eb606126b44cb0e58ce326933645272
Lcnvdl <programando.soft@gmail.com>
2020-10-08 00:12:15 -0300

    Update entry-point.js

--- entry-point.js
+++ entry-point.js
@@ -5,13 +5,13 @@ module.exports = ({ CommandBase, commands }) => {
     class NodeToolsCommand extends CommandBase {
         async run(args) {
             if (args[0] === "increase-version") {
-                return await increaseVersion();
+                return await this._increaseVersion();
             }
             else if (args[0] === "increase-and-publish") {
-                let code = await increaseVersion();
+                let code = await this._increaseVersion();
 
                 if (code === this.codes.success) {
-                    code = await publishVersion();
+                    code = await this._publishVersion();
                 }
 
                 return code;
@@ -19,18 +19,18 @@ module.exports = ({ CommandBase, commands }) => {
 
             return this.codes.invalidArguments;
         }
-    }
 
-    async function publishVersion() {
-        const pipe = new (commands.pipe)(this.environment.fork());
-        await pipe.loadFromFile(path.join(scripts, "publish-version.cmd"));
-        return this.codes.success;
-    }
-
-    async function increaseVersion() {
-        const pipe = new (commands.pipe)(this.environment.fork());
-        await pipe.loadFromFile(path.join(scripts, "increase-version.cmd"));
-        return this.codes.success;
+        async _publishVersion() {
+            const pipe = new (commands.pipe)(this.environment.fork());
+            await pipe.loadFromFile(path.join(scripts, "publish-version.cmd"));
+            return this.codes.success;
+        }
+    
+        async _increaseVersion() {
+            const pipe = new (commands.pipe)(this.environment.fork());
+            await pipe.loadFromFile(path.join(scripts, "increase-version.cmd"));
+            return this.codes.success;
+        }
     }
 
     return NodeToolsCommand;
